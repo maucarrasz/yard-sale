8f64e29f978d7487ddad4df9ad9de24df5bd15e6
Mauricio2802 <vmcarrasco2810@gmail.com>
2021-12-14 11:32:20 -0500

    Add shopping cart section in home page

--- scripts/productsInHome.js
+++ scripts/productsInHome.js
@@ -1,6 +1,8 @@
 let cards = document.querySelectorAll(".card"),
   productDetail = document.querySelector(".product-detail"),
-  closeIconContainer = productDetail.querySelector(".icon-close-container");
+  closeIconProductDetail = productDetail.querySelector(".icon-close-container"),
+  shoppingCart = document.querySelector(".cart-container"),
+  leftArrowShoppingCart = shoppingCart.querySelector(".left-arrow");
 
 cards = Array.from(cards);
 cards.forEach(e => {
@@ -11,18 +13,19 @@ cards.forEach(e => {
   cardImage.addEventListener("click", productDetailAppear);
 });
 
-closeIconContainer.addEventListener("click", closeProductDetail);
+closeIconProductDetail.addEventListener("click", closeProductDetail);
 
 function changeShoppingStatus() {
   console.log(this);
   let additionalInfo = this.parentNode;
   let card = additionalInfo.parentNode;
   let removedText = card.querySelector(".card__image-container .center-text");
-  if (!removedText.classList.contains("hidden")) {
-    this.setAttribute("src", "./images/bt_add_to_cart.svg");
+  console.log(this.getAttribute("src"));
+  if (this.getAttribute("src") === "./images/bt_add_to_cart.svg") {
+    this.setAttribute("src", "./images/bt_added_to_cart.svg");
     removedText.classList.add("hidden");
   } else {
-    this.setAttribute("src", "./images/bt_added_to_cart.svg");
+    this.setAttribute("src", "./images/bt_add_to_cart.svg");
     removedText.classList.remove("hidden");
   }
 }
@@ -33,6 +36,7 @@ function productDetailAppear() {
   let cardsContainer = card.parentNode;
   let mainContainer = cardsContainer.parentNode;
   let productDetail = mainContainer.querySelector(".product-detail");
+  console.log(productDetail);
   if (productDetail.classList.contains("hidden")) {
     productDetail.classList.remove("hidden");
   } else {
@@ -42,4 +46,19 @@ function productDetailAppear() {
 
 function closeProductDetail() {
   productDetail.classList.add("hidden");
+}
+
+let buttonAddToCart = document.getElementById("button-add-to-cart");
+buttonAddToCart.addEventListener("click", shoppingCartAppear);
+leftArrowShoppingCart.addEventListener("click", closeShoppingCart);
+
+function shoppingCartAppear() {
+  console.log(this);
+  if (shoppingCart.classList.contains("hidden")) {
+    shoppingCart.classList.remove("hidden");
+  }
+}
+
+function closeShoppingCart() {
+  shoppingCart.classList.add("hidden");
 }
