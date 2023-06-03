f8c7ee905f3bf3e1ae95511785e0eab03324053d
Mauricio2802 <vmcarrasco2810@gmail.com>
2023-06-02 20:20:27 -0500

    Bring all the components together in index.html, home.css, main.js and data.js (hardcode data). Added functionalities: 1. Add a new product to the shopping cart from the product in the home page or from the product detail section. 2. Remove products from the shopping cart. Also, add animations when adding and removing products from the shopping cart (pop-up text message).

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ const asideShoppingCart = document.querySelector(".cart-container");
 const asideCartCloseBtn = document.querySelector(".cart-container .left-arrow");
 const productDetail = document.querySelector(".product-detail");
 const productDetailCloseBtn = document.querySelector(".icon-close-container");
+const ordersCartContainer = document.querySelector(".orders-container");
+const addToCartBtn = document.getElementById("button-add-to-cart");
 
 menuAccount.addEventListener("click", menuToggleDesktop);
 mobileMenuBtn.addEventListener("click", menuToggleMobile);
@@ -15,7 +17,44 @@ menuMobileCloseBtn.addEventListener("click", menuToggleMobile);
 menuShoppingIcon.addEventListener("click", shoppingCartToggle);
 asideCartCloseBtn.addEventListener("click", shoppingCartToggle);
 productDetailCloseBtn.addEventListener("click", closeProductDetail);
+addToCartBtn.addEventListener("click", addProductToCartFromAsideDetail);
 
+function formatNumberToCurrency(number) {
+  const isDecimal = !Number.isInteger(number);
+  let decimalPart = "";
+
+  if (isDecimal) {
+    decimalPart = Math.floor((number % 1) * 100).toString();
+  }
+
+  let result = "$ " + Math.floor(number).toString();
+
+  if (isDecimal) {
+    result += "," + decimalPart;
+  } else {
+    result += ",00";
+  }
+
+  return result;
+}
+function changeOrderId(id, identifier) {
+  const slices = id.split("$");
+  slices.pop();
+  const newId = slices.join("$") + "$" + identifier;
+  return newId;
+}
+function showElement(element) {
+  element.classList.remove("hidden");
+}
+function showElementTemporarily(element, timeoutInMilliseconds) {
+  showElement(element);
+  setTimeout(function () {
+    hideElement(element);
+  }, timeoutInMilliseconds);
+}
+function hideElement(element) {
+  element.classList.add("hidden");
+}
 function menuToggleDesktop() {
   const isAsideShoppingCartOpen =
     !asideShoppingCart.classList.contains("hidden");
@@ -56,7 +95,49 @@ function shoppingCartToggle() {
 
   asideShoppingCart.classList.toggle("hidden");
 }
+function getNodeChildByParentId(parentId, querySelectorChild) {
+  const parentNode = document.getElementById(`${parentId}`);
+  const nodeChild = parentNode.querySelector(`${querySelectorChild}`);
+  return nodeChild;
+}
+function addProductToCartFromAsideDetail() {
+  const productDetail = this.parentNode.parentNode;
+  const img = productDetail.querySelector(".product__image");
+  const name = productDetail.querySelector(".product-info__name");
+  const price = productDetail.querySelector(".product-info__price");
+  const orderIdCart = changeOrderId(productDetail.getAttribute("id"), "cart");
+  const dataProductCart = {
+    imgSrc: img.src,
+    name: name.textContent,
+    price: price.textContent,
+    orderId: orderIdCart,
+  };
+  addProductToCart(dataProductCart);
+  hideElement(productDetail);
+  setTimeout(function () {
+    showElement(asideShoppingCart);
+  }, 1000);
+}
+
+function updateProductDetail(data) {
+  const img = productDetail.querySelector(".product__image");
+  const name = productDetail.querySelector(".product-info__name");
+  const price = productDetail.querySelector(".product-info__price");
+  productDetail.id = data.orderId;
+  img.src = data.imgSrc;
+  name.textContent = data.name;
+  price.textContent = data.price;
+}
 function openProductDetail() {
+  const card = this.parentNode.parentNode;
+  const dataProductDetail = {
+    imgSrc: card.querySelector(".card__image").src,
+    price: card.querySelector(".card__price").textContent,
+    name: card.querySelector(".card__name").textContent,
+    orderId: changeOrderId(card.getAttribute("id"), "detail"),
+  };
+  updateProductDetail(dataProductDetail);
+
   const isAsideShoppingCartOpen =
     !asideShoppingCart.classList.contains("hidden");
   const isMenuDesktopOpen = !desktopMenu.classList.contains("hidden");
@@ -67,36 +148,120 @@ function openProductDetail() {
   if (isMenuDesktopOpen) {
     desktopMenu.classList.add("hidden");
   }
-  productDetail.classList.remove("hidden");
+
+  setTimeout(showElement(productDetail), 500);
 }
 function closeProductDetail() {
-  productDetail.classList.add("hidden");
+  productDetail.setAttribute("id", "");
+  hideElement(productDetail);
 }
-function formatNumberToCurrency(number) {
-  const isDecimal = !Number.isInteger(number);
-  let decimalPart = "";
 
-  if (isDecimal) {
-    decimalPart = Math.floor((number % 1) * 100).toString();
+function toggleImageSrcTemporarily(img, temporalSrc, milliseconds) {
+  const oldSrc = img.src;
+  img.src = temporalSrc;
+  setTimeout(function () {
+    img.src = oldSrc;
+  }, milliseconds);
+}
+function addProductToCartFromBtnIcon() {
+  const product = this.parentNode.parentNode;
+  const productName = product.querySelector(".card__name").textContent;
+  const productPrice = product.querySelector(".card__price").textContent;
+  const productImgSrc = product.querySelector(".card__image").src;
+  const orderId = product.getAttribute("id");
+  const dataProductCart = {
+    name: productName,
+    price: productPrice,
+    imgSrc: productImgSrc,
+    orderId: changeOrderId(orderId, "cart"),
+  };
+  const addedText = product.querySelector(".added-text");
+  const btnIcon = this;
+  toggleImageSrcTemporarily(btnIcon, "../images/bt_added_to_cart.svg", 1500);
+  showElementTemporarily(addedText, 1500);
+  addProductToCart(dataProductCart);
+}
+
+function addProductToCart(data) {
+  const checkoutBtn = asideShoppingCart.querySelector("#checkout-btn");
+  const newOrder = document.createElement("div");
+  const productName = data.name;
+  const productPrice = data.price;
+  const productImgSrc = data.imgSrc;
+  const orderId = data.orderId;
+  newOrder.setAttribute("class", "order");
+  newOrder.setAttribute("id", orderId);
+
+  // Crear el elemento <figure> y <img>
+  const figureOrder = document.createElement("figure");
+  const imgOrder = document.createElement("img");
+
+  // Asignar los atributos al elemento <img>
+  imgOrder.setAttribute("src", productImgSrc);
+  imgOrder.setAttribute("alt", productName);
+  imgOrder.setAttribute("class", "order__image");
+
+  // Agregar el elemento <img> al elemento <figure>
+  figureOrder.appendChild(imgOrder);
+
+  // Crear los elementos <p> y asignar clases y contenido
+  const nameOrder = document.createElement("p");
+  nameOrder.setAttribute("class", "order__name info");
+  nameOrder.textContent = productName;
+
+  const priceOrder = document.createElement("p");
+  priceOrder.setAttribute("class", "order__price price");
+  priceOrder.textContent = productPrice;
+
+  const removedTextOrder = document.createElement("p");
+  removedTextOrder.setAttribute("class", "removed-text-cart hidden");
+  removedTextOrder.textContent = "REMOVED FROM CART";
+
+  // Crear el elemento <span> con clase
+  const closeIconOrder = document.createElement("span");
+  closeIconOrder.setAttribute("class", "close-icon");
+  closeIconOrder.addEventListener("click", removeProductFromCart);
+
+  // Agregar los elementos al nuevo elemento <div>
+  newOrder.appendChild(figureOrder);
+  newOrder.appendChild(nameOrder);
+  newOrder.appendChild(priceOrder);
+  newOrder.appendChild(removedTextOrder);
+  newOrder.appendChild(closeIconOrder);
+
+  // Agregar el nuevo elemento al documento
+  ordersCartContainer.insertBefore(newOrder, checkoutBtn);
+}
+function removeProductFromCartById(orderId) {
+  const deletedOrder = document.getElementById(`${orderId}`);
+  const parentNode = deletedOrder.parentNode;
+  parentNode.removeChild(deletedOrder);
+}
+function removeProductFromCart() {
+  const order = this.parentNode;
+  const orderContainer = order.parentNode;
+  const removedTextCart = order.querySelector(".removed-text-cart");
+  const orderName = order.querySelector(".order__name");
+  const orderPrice = order.querySelector(".order__price");
+  if (removedTextCart.classList.contains("hidden")) {
+    order.removeChild(orderName);
+    order.removeChild(orderPrice);
+    order.classList.add("remove-order");
+    showElement(removedTextCart);
+    removedTextCart.style.marginLeft = "16px";
   }
-
-  let result = "$ " + Math.floor(number).toString();
-
-  if (isDecimal) {
-    result += "," + decimalPart;
-  } else {
-    result += ",00";
-  }
-
-  return result;
+  setTimeout(function () {
+    const cartOrderId = order.getAttribute("id");
+    orderContainer.removeChild(order);
+  }, 1000);
 }
 
 function renderProducts(arr) {
-  for (product of arr) {
+  for (const product of arr) {
     const productCard = document.createElement("div");
     const imageContainerDiv = document.createElement("div");
     const imageCard = document.createElement("img");
-    const removedTextDiv = document.createElement("div");
+    const addedTextDiv = document.createElement("div");
     const additionalDiv = document.createElement("div");
     const infoDiv = document.createElement("div");
     const priceSpan = document.createElement("span");
@@ -110,11 +275,11 @@ function renderProducts(arr) {
     imageCard.classList.add("card__image");
     imageCard.addEventListener("click", openProductDetail);
 
-    removedTextDiv.classList.add("removed-text", "hidden");
-    removedTextDiv.textContent = "Removed from cart";
+    addedTextDiv.classList.add("added-text", "hidden");
+    addedTextDiv.textContent = "Added to cart";
 
     imageContainerDiv.appendChild(imageCard);
-    imageContainerDiv.appendChild(removedTextDiv);
+    imageContainerDiv.appendChild(addedTextDiv);
 
     // Crear el elemento div con la clase "card__additional"
     additionalDiv.classList.add("card__additional");
@@ -137,6 +302,7 @@ function renderProducts(arr) {
     shoppingIcon.setAttribute("src", "./images/bt_add_to_cart.svg");
     shoppingIcon.setAttribute("alt", "Shop icon");
     shoppingIcon.classList.add("card__shopping-icon");
+    shoppingIcon.addEventListener("click", addProductToCartFromBtnIcon);
 
     // Agregar el div de información y la imagen de carrito al div adicional
     additionalDiv.appendChild(infoDiv);
@@ -145,6 +311,16 @@ function renderProducts(arr) {
     // Agregar el contenedor de imágenes y el div adicional al elemento principal de la tarjeta
     productCard.appendChild(imageContainerDiv);
     productCard.appendChild(additionalDiv);
+
+    // Agregar 'id' para identificar al producto único
+    const orderId =
+      nameSpan.textContent[0] +
+      nameSpan.textContent[1] +
+      priceSpan.textContent[0] +
+      nameSpan.textContent[nameSpan.textContent.length - 2] +
+      nameSpan.textContent[nameSpan.textContent.length - 1] +
+      "$home";
+    productCard.setAttribute("id", orderId);
 
     // Agregar la tarjeta al container de productos
     const daddyContainer = document.querySelector("#cards-container");
@@ -157,18 +333,6 @@ renderProducts(productsList);
 // Handle viewport width en resize
 window.addEventListener("resize", function () {
   if (this.innerWidth < 1025) {
-    desktopMenu.classList.add("hidden");
+    hideElement(desktopMenu);
   }
 });
-
-// function toggleProductToCart() {
-//   const card = this.parentNode.parentNode;
-//   const removedText = card.querySelector(".removed-text");
-//   const cartImg = this.src;
-//   if (cartImg.indexOf("bt_add_to_cart.svg") != -1) {
-//     this.src = "./images/bt_added_to_cart.svg";
-//   } else {
-//     this.src = "./images/bt_add_to_cart.svg";
-//   }
-//   removedText.classList.toggle("hidden");
-// }
